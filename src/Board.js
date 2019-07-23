396c63bfd435334a775dd17a6122fed6ea2b9c37
karnamin <aminkarn18@gmail.com>
2019-07-23 19:40:34 +0530

    added styling

--- src/Board.js
+++ src/Board.js
@@ -67,7 +67,14 @@ class Board extends Component {
     render() {
         // if the game is won, just show a winning msg & render nothing else
         if (this.state.hasWon) {
-            return <h1>You Won!</h1>;
+            return (
+                <div className="Board-title">
+                    <div className="winner">
+                        <span className="neon-orange">YOU</span>
+                        <span className="neon-blue">WIN!</span>
+                    </div>
+                </div>
+            );
         }
 
         let tblBoard = [];
@@ -87,9 +94,15 @@ class Board extends Component {
         }
 
         return (
-            <table className="Board">
-                <tbody>{tblBoard}</tbody>
-            </table>
+            <div>
+                <div className="Board-title">
+                    <div className="neon-orange">Lights</div>
+                    <div className="neon-blue">Out</div>
+                </div>
+                <table className="Board">
+                    <tbody>{tblBoard}</tbody>
+                </table>
+            </div>
         );
     }
 }
